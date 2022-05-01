d08719ad0b403f4bc54f32fa134f678f4fa8021a
Raissa Lima <raissamclima@gmail.com>
2022-05-01 19:39:24 -0300

    primeiro projeto de git.github da dio

---
+++ projetoGit.java
@@ -0,0 +1,11 @@
+package dio;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        System.out.println(a+b);
+    }
+}
+ 
